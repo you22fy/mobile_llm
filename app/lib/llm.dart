fd70f8e95b270e33f11ad953dbbefcff4bb05f17
you22fy <uni22yu@gmail.com>
2025-12-10 18:59:09 +0900

    fix: pathの修正

--- app/lib/llm.dart
+++ app/lib/llm.dart
@@ -5,17 +5,18 @@ enum Llm {
   tinyswallow;
 
   get displayName => switch (this) {
-    Llm.gemma2 => 'google/gemma2-2b-jpt-it',
-    Llm.gemma3_1b => 'google/gemma3-1b-jpt-it',
-    Llm.gemma3_270m => 'google/gemma3-270m-jpt-it',
-    Llm.tinyswallow => 'SakanaAI/TinySwallow-1.5B',
+    Llm.gemma2 => 'gemma2-2b-jpt-it',
+    Llm.gemma3_1b => 'gemma3-1b-it',
+    Llm.gemma3_270m => 'gemma3-270m-it',
+    Llm.tinyswallow => 'tinyswallow-1.5b',
   };
 
   get assetPath => switch (this) {
     Llm.gemma2 => 'assets/artifacts/gemma-2-2B-it-BF16-Q4_K_M.gguf',
     Llm.gemma3_1b => 'assets/artifacts/gemma-3-1B-it-BF16_Q4_K_M.gguf',
     Llm.gemma3_270m => 'assets/artifacts/gemma-3-270M-BF16-Q4_K_M.gguf',
-    Llm.tinyswallow => 'assets/artifacts/Qwen2.5-1.5B-Instruct-BF16-Q4_K_M.gguf',
+    Llm.tinyswallow =>
+      'assets/artifacts/Qwen2.5-1.5B-Instruct-BF16-Q4_K_M.gguf',
   };
 }
 

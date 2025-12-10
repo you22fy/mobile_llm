a14da8b1a6e3ba135d5a8e83db1a0acd69cd3a70
you22fy <uni22yu@gmail.com>
2025-12-10 13:18:56 +0900

    feat: Gemmaの追加

--- app/lib/llm.dart
+++ app/lib/llm.dart
@@ -3,7 +3,10 @@ enum Llm {
   tinyswallow,
   gemma270mQ4KM,
   tinyswallowQ4KM,
-  qwen06bq8;
+  qwen06bq8,
+  gemma3_1bit,
+  gemma3_1bitQ4KM,
+  gemma2_2BQ4KM;
 
   get displayName => switch (this) {
     Llm.gemma270m => 'Gemma 270M',
@@ -11,6 +14,9 @@ enum Llm {
     Llm.gemma270mQ4KM => 'Gemma 270M Q4KM',
     Llm.tinyswallowQ4KM => 'TinyLlama 3.70B Q4KM',
     Llm.qwen06bq8 => 'Qwen 0.6B Q8',
+    Llm.gemma3_1bit => 'Gemma 3 1B it',
+    Llm.gemma3_1bitQ4KM => 'Gemma 3 1B it Q4KM',
+    Llm.gemma2_2BQ4KM => 'Gemma 2 2B Q4KM',
   };
 
   get assetPath => switch (this) {
@@ -20,6 +26,9 @@ enum Llm {
     Llm.tinyswallowQ4KM =>
       'assets/artifacts/Qwen2.5-1.5B-Instruct-BF16-Q4_K_M.gguf',
     Llm.qwen06bq8 => 'assets/artifacts/Qwen3-0.6B-Q8_0.gguf',
+    Llm.gemma3_1bit => 'assets/artifacts/gemma-3-1B-it-BF16.gguf',
+    Llm.gemma3_1bitQ4KM => 'assets/artifacts/gemma-3-1B-it-BF16_Q4_K_M.gguf',
+    Llm.gemma2_2BQ4KM => 'assets/artifacts/gemma-2-2B-it-BF16-Q4_K_M.gguf',
   };
 }
 
